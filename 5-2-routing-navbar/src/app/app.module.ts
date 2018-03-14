e97e1aa04e7ba6552bf465765c6b7113984e22ff
Ezequiel Llarena Borges <ezequiel.formacion@gmail.com>
2018-03-14 12:56:35 +0100

    Add files via upload

---
+++ 5-2-routing-navbar/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+/* */ import { Routes, RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { ProveedoresService } from './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+
+/* */ const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: '**', component: InicioComponent} /* Cambiar por 404 */
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [ProveedoresService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
