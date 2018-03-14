24b650d6025c0681f5f98beac2e1295361a9f6f3
Ezequiel Llarena Borges <ezequiel.formacion@gmail.com>
2018-03-14 12:55:21 +0100

    Add files via upload

---
+++ 5-1-routing-implementacion/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+/* */ import { Routes, RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { ProveedoresService } from './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: '**', component: InicioComponent} /* Cambiar por 404 */
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [ProveedoresService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
