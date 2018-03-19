62998504b928c7be50d5f1884703eff94931b6e7
Ezequiel Llarena Borges <ezequiel.formacion@gmail.com>
2018-03-19 16:28:46 +0100

    Add files via upload

---
+++ 5 Servicios/creacion-servicio/src/app/servicios/proveedores.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProveedoresService {
+
+  constructor() { }
+  getProveedores() {
+    return 'Mensaje desde el Servicio'; 
+  }
+}
